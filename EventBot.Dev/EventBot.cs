85c06398c871ea190db9ef2f4b517c3a86544462
Brendon Tse <brendonjtse@gmail.com>
2019-12-09 10:14:46 -0800

    debug C# version, .env still WIP

--- EventBot.Dev/EventBot.cs
+++ EventBot.Dev/EventBot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Discord;
 using Discord.Commands;
@@ -11,31 +12,37 @@ using Discord.WebSocket;
 
 
 //Functions------------------------------------------------------------------------------------------------
-public class EventBot
+namespace EventBot
 {
-	private DiscordSocketClient _client;
-
-	public static void Main(string[] args)
-		=> new EventBot().MainAsync().GetAwaiter().GetResult();
-
-	public async Task MainAsync()
+	public class EventBot
 	{
-        _client = new DiscordSocketClient();
 
-        _client.Log += Log;
+		private DiscordSocketClient _client;
 
-        var token = Environment.GetEnvironmentVariable(".env");
-    
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
+		public static void Main(string[] args)
+			=> new EventBot().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+
+			_client.Log += Log;
+
+			string token = "TOKEN_GOES_HERE";
+			// string token = Environment.GetEnvironmentVariable(".env");
+		
+			await _client.LoginAsync(TokenType.Bot, token);
+			await _client.StartAsync();
 
 
-        await Task.Delay(-1);
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
 	}
 
-    private Task Log(LogMessage msg)
-	{
-		Console.WriteLine(msg.ToString());
-		return Task.CompletedTask;
-	}
 }
