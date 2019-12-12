c0c8075e4f71210a9f1dc0815291518d219c46ff
Brendon Tse <brendonjtse@gmail.com>
2019-12-11 16:50:52 -0800

    begin building command functionality

---
+++ EventBot.Dev/Commands/Commands.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Addons.Interactive;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventBotCommands
+{
+    class BotCommands
+    {
+        private CommandService _commands;
+        private IServiceProvider _service;
+
+        public BotCommands(DiscordSocketClient client)
+        {
+            // CommandService initializes a new CommandService class with the provided configuration (CommandServiceConfig).
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+                DefaultRunMode = RunMode.Async
+            });
+
+            _service = new ServiceCollection()
+            .AddSingleton(client)
+            .AddSingleton<InteractiveService>()
+            .BuildServiceProvider();
+        }
+
+        public async Task ExecuteCommand(SocketCommandContext context, int argPosition)
+        {
+            var result = await _commands.ExecuteAsync(context, argPosition, _service);
+            
+            if (!result.IsSuccess) {
+                if (result.Error.Value == CommandError.UnmetPrecondition) 
+                {
+                    await context.Channel.SendMessageAsync("You can't do that");
+                } else if (result.Error.Value == CommandError.BadArgCount || result.Error.Value == CommandError.ParseFailed || result.Error.Value == CommandError.ObjectNotFound) {
+                    await context.Channel.SendMessageAsync("Something went wrong");
+                }
+            }
+        } 
+    }
+}
---
+++ EventBot.Dev/Commands/CoreCommands.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Addons.Interactive;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventBotCommands
+{
+    // ModuleBase provides a base class for a command module to inherit from.
+    // SocketCommandContext represents a WebSocket-based context of a command. This may include the client, guild, channel, user, and message.
+    public class CoreCommands : ModuleBase<SocketCommandContext>
+    {
+        private CommandService _service;
+
+        public CoreCommands(CommandService service)
+        {
+            _service = service;
+        }
+
+        [Command("hello"), Alias("hi"), Summary("Says Hello")]
+        public async Task Hello()
+        {
+            await Context.Channel.SendMessageAsync($"Hello {Context.User.Username}");
+        }
+    }
+}
